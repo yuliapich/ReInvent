26fbf1cb2b74ff5c10b32b85ecc1b9754fb9b1f5
Yulia Pichugina <julia-ch.s@list.ru>
2025-07-31 11:25:04 +0300

    Initial Commit

---
+++ ReInventTests/ReInventTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReInventTests.swift
+//  ReInventTests
+//
+//  Created by Yulia Pichugina on 31/07/2025.
+//
+
+import Testing
+@testable import ReInvent
+
+struct ReInventTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
